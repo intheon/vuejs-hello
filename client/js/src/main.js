92039a1fc19371b541700df1f33baee744e97314
Ben S <allobon@gmail.com>
2016-06-16 12:31:39 +0100

    dummy boilerplate

---
+++ client/js/src/main.js
@@ -0,0 +1,18 @@
+import {$} from "jquery";
+import Material from "materialize-css";
+import Vue from "vue";
+import VueResource from "vue-resource";
+
+
+new Vue({
+	el: "#noticeApp",
+	data: {
+		event: {
+			name: "",
+			desc: ""
+		}
+	},
+	ready: function(){},
+	methods: {}
+})
+
